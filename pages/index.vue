bfcdea0b4ec1e6e206999e266ecc66cc7eb2233e
ayazadeh <m.ayazadeh@gmail.com>
2025-05-04 15:51:48 +0330

    Feat: add vee-validate & yup

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,17 @@
 			Primary
 			<BeakerIcon class="size-6" />
 		</button>
-        <br>
-        x: {{ x }}
+		<br />
+		x: {{ x }}
 
-        <div class="box bg-red-400 size-4"></div>
-        <div class="box bg-green-400 size-4"></div>
+		<div class="box bg-red-400 size-4"></div>
+		<div class="box bg-green-400 size-4"></div>
+		<form @submit="onSubmit">
+			<input v-model="field" v-bind="fieldProps" class="input" />
+			<span class="text-red-500">{{ errors.field }}</span>
+
+			<button class="btn">Submit</button>
+		</form>
 	</div>
 </template>
 
@@ -19,6 +25,7 @@ import { onMounted } from "vue";
 import { BeakerIcon } from "@heroicons/vue/24/solid";
 import { useMouse } from "@vueuse/core";
 import { gsap } from "gsap";
+import { useForm } from "vee-validate";
 
 definePageMeta({
 	layout: "custom",
@@ -29,9 +36,30 @@ const { x, y } = useMouse();
 
 onMounted(() => {
 	gsap.to(".box", {
-        rotation: 27,
+		rotation: 27,
 		x: 100,
 		duration: 2,
 	});
+});
+
+// Validation, or use `yup` or `zod`
+function required(value) {
+	return value ? true : "This field is required";
+}
+
+// Create the form
+const { defineField, handleSubmit, errors } = useForm({
+	validationSchema: {
+		field: required,
+	},
+});
+
+// Define fields
+const [field, fieldProps] = defineField("field");
+
+// Submit handler
+const onSubmit = handleSubmit((values) => {
+	// Submit to API
+	console.log(values);
 });
 </script>
