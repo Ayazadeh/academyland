65dc65f332211d12ccf905745dc8cda41eb9a056
ayazadeh <m.ayazadeh@gmail.com>
2025-05-04 15:30:54 +0330

    Feat: add @vueuse/core

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="text-center">
-        <ThemeSelector />
-        <h1>page</h1>
-        <button class="btn btn-secondary">
-            Secondary
-            <BeakerIcon class="size-6" />
-        </button>
-    </div>
+	<div class="text-center">
+		<ThemeSelector />
+		<h1>page</h1>
+		<button class="btn btn-primary">
+			Primary
+			<BeakerIcon class="size-6" />
+		</button>
+        <br>
+        x: {{ x }}
+	</div>
 </template>
 
 <script setup>
-import { BeakerIcon } from '@heroicons/vue/24/solid'
+import { BeakerIcon } from "@heroicons/vue/24/solid";
 
 definePageMeta({
-    layout: 'custom'
-})
+	layout: "custom",
+});
+
+import { useLocalStorage, useMouse, usePreferredDark } from "@vueuse/core";
+
+// tracks mouse position
+const { x, y } = useMouse();
+
+// is user prefers dark theme
+const isDark = usePreferredDark();
+
+// persist state in localStorage
+const store = useLocalStorage("my-storage", {
+	name: "Apple",
+	color: "red",
+});
 </script>
