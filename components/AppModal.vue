aa9da7307ab69dd029a06eb629947fc38e88f97d
ayazadeh <m.ayazadeh@gmail.com>
2025-05-05 13:19:56 +0330

    Feat: add AppModal

---
+++ components/AppModal.vue
@@ -0,0 +1,61 @@
+<template>
+    <input 
+        v-model="checkboxModel"
+        type="checkbox"
+        class="modal-toggle"
+    />
+    <div 
+        class="modal"
+        @click="closeModal"
+    >
+        <div 
+            v-if="eager || modelValue"
+            class="modal-box relative overflow-hidden"
+            @click.stop
+        >
+            <section class="flex items-center justify-between">
+                <span class="font-medium">{{ title }}</span>
+                <span 
+                    class="cursor-pointer text-xl select-none text-gray-500" 
+                    @click="closeModal"
+                >&#x2715;</span>
+            </section>
+            <div class="overflow-y-auto max-h-[85vh] lg:max-h-[80vh] scrollbar-thin"> 
+                <slot />
+            </div>
+            <div
+                v-if="$slots.actions"
+                class="modal-action"
+            >
+                <slot name="actions" />
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const props = withDefaults(defineProps<{
+    modelValue: boolean;
+    eager?: boolean;
+    title?: string;
+}>(), {
+    modelValue: false,
+    eager: false,
+    title: ''
+})
+
+const emits = defineEmits(['update:modelValue']);
+
+const checkboxModel = ref(!!props.modelValue)
+
+watch(() => props.modelValue, (val) => {
+    checkboxModel.value = !!val
+})
+
+const closeModal = () => {
+    emits('update:modelValue', false)
+}
+</script>
--- pages/base.vue
+++ pages/base.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="flex flex-col space-y-3 justify-center items-center text-center">
+    <div class="flex flex-col space-y-3 justify-center items-center text-center h-screen">
         <h1>my base component</h1>
-        <AppButton :variant="ButtonVariantEnum.SECONDARY" loading>دکمه</AppButton>
+        <AppModal v-model="isModalOpen">
+            <div>
+                <h2>Modal Content</h2>
+                <p>This is the content of the modal.</p>
+            </div>
+            
+        </AppModal>
+        <AppButton 
+            :variant="ButtonVariantEnum.SECONDARY" 
+            @click="isModalOpen = true"
+        >
+            <span>دکمه</span>
+        </AppButton>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { ButtonVariantEnum } from "@/types";
+
+const isModalOpen = ref(false);
 </script>
-
-<style lang="scss" scoped>
-
-</style>
