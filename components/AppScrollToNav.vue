9cd502d432b1ee40ce41fbb8756ac56cdcf8a93b
ayazadeh <m.ayazadeh@gmail.com>
2025-05-06 11:06:42 +0330

    Feat: add AppScrollToNav and slider page

---
+++ components/AppScrollToNav.vue
@@ -0,0 +1,88 @@
+<template>
+    <AppSlider
+        :items="items"
+        item-container-class="flex-shrink-0"
+        :middle="activeItemIndex"
+        :arrows="false"
+    >
+        <template #item="{ label, id, index }">
+            <div 
+                role="button"
+                :class="{ 'text-primary': index === activeItemIndex }"
+                @click="scrollToID(id)"
+            >
+                {{ label }}
+            </div>
+        </template>
+    </AppSlider>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { gsap } from 'gsap';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+import { useWindowScroll } from '@vueuse/core';
+
+const props = withDefaults(defineProps<{
+    items: { id: string, label: string }[];
+    offset?: number;
+}>(), {
+    offset: 300,
+})
+
+const route = useRoute();
+const activeHash = ref(route.hash || '');
+
+const updateHash = (id: string) => {
+    if (import.meta.client) {
+        window.history.pushState(null, '', '#' + id);
+        activeHash.value = '#' + id;
+    }
+}
+
+onMounted(() => {
+    gsap.registerPlugin(ScrollToPlugin);
+    if (route.hash) {
+        gsap.to(window, {
+            duration: 1,
+            scrollTo: { 
+                y: route.hash,
+                offsetY: props.offset,
+            }
+        })
+    }
+})
+
+let scrollTo: any;
+const scrollToID = (id: string) => {
+    updateHash(id);
+    scrollTo = gsap.to(window, {
+        duration: 1,
+        scrollTo: {
+            y: '#' + id,
+            offsetY: props.offset,
+            autoKill: true,
+        }
+    })
+}
+
+const { y } = useWindowScroll();
+watch(y, () => {
+    for (let i = props.items.length - 1; i >= 0; i--) {
+        const item = props.items[i];
+        const el = document.getElementById(item.id);
+        if (el && el?.getBoundingClientRect().top <= props.offset + 2) {
+            updateHash(item.id);
+            return;
+        }
+        if (i == 0) {
+            updateHash('');
+        }
+    }
+})
+
+const activeItemIndex = computed(() => {
+    return props.items.findIndex(item => item.id === unref(activeHash)); 
+})
+</script>
---
+++ pages/slider.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col space-y-3 justify-center items-center">
+        <div class="sticky top-0 bg-white py-4 z-10">
+            <AppScrollToNav :items="CourseTabs"></AppScrollToNav>
+        </div>
+        <template v-for="item in CourseTabs" :key="`course-item-${item.id}`">
+            <div class="h-96">
+                <h2 :id="item.id">{{ item.label }}</h2>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script setup lang="ts">
+const CourseTabs = [
+    { id: 'about-course', label: 'درباره دوره' },
+    { id: 'requirements-course', label: 'پیش نیازهای دوره' },
+    { id: 'chapters-course', label: 'لیست ویدئوها' },
+    { id: 'faqs-course', label: 'سوالات متداول' },
+    { id: 'comments-course', label: 'نظرات کاربران' },
+]
+</script>
+
