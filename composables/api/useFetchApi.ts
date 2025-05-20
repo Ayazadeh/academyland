fa279224a281eec5fedb427c1f951f94b277c34b
ayazadeh <m.ayazadeh@gmail.com>
2025-05-20 10:57:01 +0330

    Ref: move plainToClass to useFetchApi.ts

--- composables/api/useFetchApi.ts
+++ composables/api/useFetchApi.ts
@@ -1,11 +1,20 @@
 import type { FetchOptions } from 'ofetch'
+import { plainToClass } from "class-transformer";
+import type { ClassConstructor } from "class-transformer";
 
-export const useFetchApi = () => {
+export const useFetchApi =<T extends ClassConstructor<unknown>>(classTransformer: T | null = null) => {
     const myCustomFetch = (url: string, config?: FetchOptions) => {
         config = { baseURL: 'https://acm.academyland.net/api/web', ...config }
         return $fetch(url, {
             ...config,
             method: config.method?.toUpperCase() as 'GET' | 'HEAD' | 'PATCH' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE'
+        }).then((response) => {
+            if (classTransformer != null) {
+                return plainToClass(classTransformer, response, { 
+                    excludeExtraneousValues: true 
+                });
+            }
+            return response;
         })
     }
     return myCustomFetch
--- composables/course/useCourse.service.ts
+++ composables/course/useCourse.service.ts
@@ -1,23 +1,16 @@
 import { useFetchApi } from "~/composables/api/useFetchApi";
 import { CourseDto } from "./course.dto";
-import { plainToClass, plainToInstance } from "class-transformer";
 
 export const useCourseService = () => {
-    const $fetch = useFetchApi();
+    const $fetch = useFetchApi(CourseDto);
     return async () => {
         try {
             const response = await $fetch('/course/index', { 
                 params: { expand: 'src, statusText, isRecording, percent' }
             });
 
-            console.log('test ', response);
-            
-            
-            // Convert response to plain object if it's not already
-            const plainResponse = Object.assign({}, response);
-            return plainToInstance(CourseDto, plainResponse, { 
-                excludeExtraneousValues: true 
-            });
+            return response;
+
         } catch (error) {
             console.error('Error fetching courses:', error);
             throw error;
