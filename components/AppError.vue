5a2ee4c0d6c29e17865b974d3804c41ee12e889c
ayazadeh <m.ayazadeh@gmail.com>
2025-05-28 09:09:55 +0330

    Feat: add AppError

---
+++ components/AppError.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        v-if="message"
+        class="text-red-500 bg-red-100 prose-sm rounded border-red-300 p-3"
+        :class="{ 'text-center': center }"
+    >
+        <slot>
+            {{ message }}
+        </slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    message?: string;
+    center?: boolean;
+}>()
+</script>
