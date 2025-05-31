64a9c48bafbfda80e207aae63e6079e9f227532e
ayazadeh <m.ayazadeh@gmail.com>
2025-05-31 09:03:05 +0330

    Feat: add login.service.ts

--- composables/api/api.config.ts
+++ composables/api/api.config.ts
@@ -10,5 +10,5 @@ export const PROXY_CONFIG = isProd ? {} : {
     }
 }
 
-export const CLIENT_ID = ''
-export const CLIENT_SECRET = ''
+export const CLIENT_ID = 'fkgrr3w3@gdgdjk'
+export const CLIENT_SECRET = 'ghgkhgh@bfb,fbfgfgasd'
---
+++ composables/auth/login/login.service.ts
@@ -0,0 +1,46 @@
+import { useFetchApi } from "~/composables/api/useFetchApi";
+import { CLIENT_ID, CLIENT_SECRET } from "~/composables/api/api.config";
+import { useLoginValidator } from "./login.validator";
+import type { AuthTokens, Identity } from "../Auth.interface";
+import type { InferType } from "yup";
+
+export const useLoginService = () => {
+	const fetchData = useFetchApi();
+    const { schema } = useLoginValidator()
+
+	const login = async ({
+		username,
+		password
+	}: InferType<typeof schema>): Promise<{ tokens: AuthTokens; identity: Identity } | undefined > => {
+        try {
+            const response: any = await fetchData('/oauth2/rest/token', {
+                method: 'post',
+                body: {
+                    username,
+                    password,
+                    grant_type: 'password',
+                    client_id: CLIENT_ID,
+                    client_secret: CLIENT_SECRET
+                }
+            })
+
+            if (response !== undefined) {
+                const { access_token, refresh_token, expires_in, identity } = response
+                return {
+                    tokens: {
+                        accessToken: access_token,
+                        refreshToken: refresh_token,
+                        expiresIn: new Date().getTime() + expires_in * 1000
+                    },
+                    identity
+                }
+            }
+            return response
+
+        } catch(e) {
+            console.error(e)
+        }
+    };
+
+	return { login };
+};
